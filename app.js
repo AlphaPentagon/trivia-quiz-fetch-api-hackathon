dbc8eb48d88c820c892a1f384157e59382ab3140
ZainabAbid1 <103643063+ZainabAbid1@users.noreply.github.com>
2022-05-06 11:41:20 +0100

    js file attempt 1

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 
     
+    
+    // Fetch the questions 
+
+    let questions;
     async function getQuestions() {
         let response = await fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple");
-        let questions = await response.json();
+         questions = await response.json();
         console.log(questions);
+        
         return questions;
     }
+// We have some code that works 
+   getQuestions().then( questions => {
+    console.log(questions.results[0].question);
+    let question= document.querySelector("#question");
+    question.innerText= questions.results[0].question;
+  })
+// Getting the elements 
+// console log 1 question
+// while the promise is in a state of resolve 
+//console.log(questions.results[0].question);
+  // Set the inner Text to be the current question
+// let question= document.querySelector("#question");
+// question.innerText= questions.results[0].question;
+// Select the Question using our question id 
+
+// Function
+    // Set the inner Text to be the current question
+    // Fetch one random question from the array of results
+    // Put the other possible answers in the buttons 
+    // if statement to check if the answer matches correct answer
+        // tell if the answer is right or wrong and display it
+    // function that updates the score and displays it        
+    // function for timer 
+    // function for feedback
+
 
     
     
