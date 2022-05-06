ee95319146bbd8116822ee261651698c89d4586a
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-05-06 12:13:58 +0100

    selected elements, started setting up event listeners on buttons

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
 
-    
-    
-    // Fetch the questions 
+    // Getting the elements 
+    let question= document.querySelector("#question");
+    let title = document.querySelector("#title");
+    let timer = document.querySelector("#timer");
+    let difficulty = document.querySelector("#difficulty");
+    let answerButtonA = document.querySelector("#answerA");
+    let answerButtonB = document.querySelector("#answerB");
+    let answerButtonC = document.querySelector("#answerC");
+    let answerButtonD = document.querySelector("#answerD");
+    let score = document.querySelector("#score");
+    let feedback = document.querySelector("#feedback");
 
+
+    // Declare global variables
     let questions;
-    async function getQuestions() {
+    let timerCount = 10;
+    let scoreCount = 0;
+    let playerAnswer;
+    let correctAnswer;
+
+    
+
+    // Fetch the questions 
+    async function getQuestionInformation() {
         let response = await fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple");
          questions = await response.json();
-        console.log(questions);
-        
+        console.log(questions);        
         return questions;
-    }
+    };
+
 // We have some code that works 
-   getQuestions().then( questions => {
-    console.log(questions.results[0].question);
-    let question= document.querySelector("#question");
+    // displays the question on the page
+   getQuestionInformation().then( questions => {   
+    // Set the inner Text to be the current question    
     question.innerText= questions.results[0].question;
-  })
-// Getting the elements 
-// console log 1 question
-// while the promise is in a state of resolve 
-//console.log(questions.results[0].question);
-  // Set the inner Text to be the current question
-// let question= document.querySelector("#question");
-// question.innerText= questions.results[0].question;
-// Select the Question using our question id 
+    // Gets the correct answer and store it in a variable
+    correctAnswer = questions.results[0].correct_answer;
+  });
+
+// Put event listeners on each button
+answerButtonA.addEventListener("click", function() { checkAnswer("A")});
+  
+// Put the possible answers in the buttons
+
+ // if statement to check if the answer matches correct answer
+    // tell if the answer is right or wrong and display it 
+    function checkAnswer(answer) {
+
+    }
 
 // Function
-    // Set the inner Text to be the current question
-    // Fetch one random question from the array of results
+    // Set the inner Text to be the current question ✅
+    // Phase 2: Fetch one random question from the array of results
     // Put the other possible answers in the buttons 
     // if statement to check if the answer matches correct answer
         // tell if the answer is right or wrong and display it
